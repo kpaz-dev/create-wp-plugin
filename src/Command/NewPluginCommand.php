41a2ab05ce8a41442fdc2a25d14c8877114b7a69
Kevin Paz <pazortizkevindaniel@gmail.com>
2026-02-24 12:00:39 +0100

    Add WordPress plugin CLI tool
    - Initialize `composer.json` with PSR-4 autoloading, author details, and `symfony/console` dependency.
    - Add a CLI entry point in `bin/wp-plugin`.
    - Implement `NewPluginCommand` to scaffold WordPress plugins.

---
+++ src/Command/NewPluginCommand.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Kpaz\CreateWpPlugin\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class NewPluginCommand extends Command
+{
+    protected static $defaultName = 'new';
+
+    protected function configure(): void
+    {
+        $this->setDescription('Create a new WP plugin')
+        ->addArgument('name', InputArgument::REQUIRED, 'Plugin name');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $name = $input->getArgument('name');
+
+        if(is_dir($name)) {
+            $output->writeln("<error>Directory already exists</error>");
+            return Command::FAILURE;
+        }
+
+        mkdir($name);
+        mkdir($name . '/src');
+
+        $pluginFile = "$name/$name.php";
+
+        file_put_contents(
+            $pluginFile,
+            "<?php\n\n/**\n * Plugin Name: $name\n */\n"
+        );
+
+        $output->writeln("<info>Plugin created successfully!</info>");
+
+        return Command::SUCCESS;
+    }
+
+}
